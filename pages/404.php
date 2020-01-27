88ce940e05e2e7f5eb358ef19dd6f630a2747223
Cloudy Young <cloudyyoung74@outlook.com>
2020-01-27 15:07:05 -0700

    Update 404.php

--- pages/404.php
+++ pages/404.php
@@ -3,6 +3,7 @@
 
 $lyrics = [
     "But we were dancing, dancing with our hands tied, hands tied.",
+    "Not a flower on the wall, I am growing ten feet ten feet tall.",
     "In your fantasy, dream about me, and all that we could do this emotion.",
     "When I'm close to you, we blend into, my favourite color.",
     "Worlds fly by, drove by your place and stopped again tonight.",
@@ -14,6 +15,12 @@ $lyrics = [
     "I feel the cool wind coming, end before the summer rain.",
     "I see the sky staring into the blue like it's not gonna end.",
     "Is anyone there? Oh-oh. Play.",
+    "Stars, won't you shine on me, won't you dance with me, me and my baby.",
+    "Sweet like the morning light goes late into the evening.",
+    "I never get to hold you, as long as I want to.",
+    "I know you've had a rough time, here I've come to hijack you, hijack you.",
+    "Taking off, got your hands in the air and ya feeling alright.",
+    "Planes I'm hopping, cards I'm dropping, no shop can fill me up.",
 ];
 
 $rand = rand(0, count($lyrics) - 1);
